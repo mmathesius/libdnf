95a35a8dad6e81819e73e92dcec8ab85dbd281cf
Rafael Fonseca <rdossant@redhat.com>
2018-07-23 19:16:26 +0200

    modularity: add exception classes
    Signed-off-by: Rafael Fonseca <rdossant@redhat.com>

--- libdnf/dnf-module.h
+++ libdnf/dnf-module.h
@@ -27,6 +27,22 @@
 
 namespace libdnf {
 
+struct ModuleException : public std::runtime_error {
+    explicit ModuleException(const std::string &what) : std::runtime_error(what) {}
+};
+
+class ModuleExceptionList : public std::exception {
+public:
+    explicit ModuleExceptionList() {}
+    const std::vector<ModuleException> & list() const noexcept { return _list; }
+    void add(const ModuleException &what) { _list.push_back(what); }
+    void add(const std::string &what) { _list.push_back(ModuleException(what)); }
+    bool empty() { return _list.empty(); }
+
+private:
+    std::vector<ModuleException> _list;
+};
+
 bool dnf_module_dummy(const std::vector<std::string> & module_list);
 bool dnf_module_enable(const std::vector<std::string> & module_list);
 
